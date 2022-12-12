2f439f74f5d02cd160e52c9e443451ea0f6aaf2b
Haltarys <45515869+Haltarys@users.noreply.github.com>
2022-12-12 23:37:44 +0100

    feat(run.sh): rename and fix script

--- log.sh
+++ log.sh
@@ -7,8 +7,9 @@ fi
 
 # Run and log results
 logfile="$HOME/twitch-drops-watcher.log"
-gamenames=("Mortal Shell" "Mortal Shell: Enhanced Edition") # Or whatever games you prefer
+defaults=("Mortal Shell" "Mortal Shell: Enhanced Edition") # Default values
+games=("${@:-"${defaults[@]}"}")
 
 echo $(date) >> $logfile
-(sudo docker run --rm twitch-drops-watcher $gamenames --drops 2>&1) >> $logfile
+(sudo docker run --rm twitch-drops-watcher "${games[@]}" --drops 2>&1) >> $logfile
 echo >> $logfile
