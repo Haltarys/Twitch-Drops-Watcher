8855e1bb49b805903e7e45ece050188505a33fd4
Haltarys <45515869+Haltarys@users.noreply.github.com>
2022-12-12 23:37:44 +0100

    fix(log): update logging script

--- log.sh
+++ log.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-# Build if image does not exist
-if [[ "$(sudo docker images -q twitch-drops-watcher 2> /dev/null)" == "" ]]; then
-  sudo docker build -t twitch-drops-watcher .
-fi
-
 # Run and log results
 logfile="$HOME/twitch-drops-watcher.log"
+envfile="$HOME/twitch-drops-watcher.env"
 defaults=("Mortal Shell" "Mortal Shell: Enhanced Edition") # Default values
 games=("${@:-"${defaults[@]}"}")
 
 echo $(date) >> $logfile
-(sudo docker run --rm twitch-drops-watcher "${games[@]}" --drops 2>&1) >> $logfile
+(npx twitch-drops-watcher "${games[@]}" --drops --env $envfile 2>&1) >> $logfile
 echo >> $logfile
