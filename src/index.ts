3be88bb2de0e29b167bcbcbd3a9e34b8558d83f6
Haltarys <45515869+Haltarys@users.noreply.github.com>
2022-12-12 23:37:44 +0100

    feat: add extra command line arguments

--- src/index.ts
+++ src/index.ts
@@ -4,27 +4,49 @@ import TwitchAPIClient from './TwitchAPIClient';
 // Import env variables from .env file
 dotenv.config();
 
-async function main() {
-  const clientId = process.env.TWITCH_CLIENT_ID!;
-  const clientSecret = process.env.TWITCH_CLIENT_SECRET!;
-  const gameNames = process.env.GAME_NAMES!.split('|');
+function printHelp(): void {
+  console.log(
+    [
+      'USAGE: node . [OPTION]... GAMES...',
+      '\t -d, --drops\n\t\tOnly show streams with Twitch Drops enabled',
+      '\t -h, --help\n\t\tShow this help and exit',
+    ].join('\n')
+  );
+}
 
-  const client = await TwitchAPIClient.create(clientId, clientSecret);
-
-  const liveStreams = await client.getGameIds(gameNames).then((gameIds) => client.getLiveStreams(gameIds, true));
-  if (liveStreams.length > 0) {
-    liveStreams.forEach((stream) => {
-      console.log(
-        `${stream.user_name} is playing ${stream.game_name} live at ${TwitchAPIClient.buildTwitchURL(stream)}!`
-      );
-    });
+async function main(): Promise<void> {
+  if (process.argv.some((arg) => arg === '-h' || arg === '--help')) {
+    printHelp();
   } else {
-    console.log(
-      [
-        `No one is live for the following games: ${gameNames}.`,
-        "Check the game names, disable the 'Drops Enabled' tag, or come back later.",
-      ].join('\n')
-    );
+    const dropsEnabled = process.argv.some((arg) => arg === '-d' || arg === '--drops');
+    const gameNames = process.argv.slice(2).filter((arg) => arg[0] != '-');
+
+    if (gameNames.length === 0) {
+      console.error('Please pass at least one game name as a command line argument.');
+      return;
+    }
+
+    const clientId = process.env.TWITCH_CLIENT_ID!;
+    const clientSecret = process.env.TWITCH_CLIENT_SECRET!;
+    const client = await TwitchAPIClient.create(clientId, clientSecret);
+
+    const liveStreams = await client
+      .getGameIds(gameNames)
+      .then((gameIds) => client.getLiveStreams(gameIds, dropsEnabled));
+    if (liveStreams.length > 0) {
+      liveStreams.forEach((stream) => {
+        console.log(
+          `${stream.user_name} is playing ${stream.game_name} live at ${TwitchAPIClient.buildTwitchURL(stream)}!`
+        );
+      });
+    } else {
+      console.log(
+        [
+          `No one is live for the following games: ${gameNames}.`,
+          "Check the game names, disable the 'Drops Enabled' tag, or come back later.",
+        ].join('\n')
+      );
+    }
   }
 }
 
