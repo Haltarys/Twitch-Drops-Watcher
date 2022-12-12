044cee6d659f12a2d08f0fa900fe5b63d4e7b080
Haltarys <45515869+Haltarys@users.noreply.github.com>
2022-12-12 23:37:44 +0100

    feat: get live streams and filter with tags

---
+++ src/TwitchAPIClient.ts
@@ -0,0 +1,74 @@
+import axios, { AxiosInstance } from 'axios';
+import type { Credentials, Game, TwitchStream } from './entities';
+
+export default class TwitchAPIClient {
+  private client: AxiosInstance;
+
+  /**
+   * Constructs a TwitchAPIClient instance from a client ID and credentials.
+   * @param clientId The client ID from the Twitch API.
+   * @param credentials An object returned by the Twitch API containing an access token.
+   */
+  private constructor(clientId: string, credentials: Credentials) {
+    this.client = axios.create({
+      baseURL: 'https://api.twitch.tv/helix',
+      headers: {
+        Authorization: `Bearer ${credentials.access_token}`,
+        'Client-Id': clientId,
+      },
+    });
+  }
+
+  /**
+   * Asynchronously creates a TwitchAPIClient with a client ID and a secret from the Twitch API.
+   * @param clientId The client ID from the Twitch API.
+   * @param clientSecret The client secret from the Twitch API.
+   * @returns An asynchronously created TwitchAPIClient instance.
+   */
+  static async create(clientId: string, clientSecret: string): Promise<TwitchAPIClient> {
+    return axios
+      .post(
+        'https://id.twitch.tv/oauth2/token',
+        `client_id=${clientId}&client_secret=${clientSecret}&grant_type=client_credentials`,
+        { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
+      )
+      .then((res) => new TwitchAPIClient(clientId, res.data));
+  }
+
+  /**
+   * Retrieves the Twitch IDs of an array of videogame names.
+   * @param gameNames An array of videogame names.
+   * @returns An array of videogame Twitch IDs.
+   */
+  async getGameIds(gameNames: string[]): Promise<string[]> {
+    return this.client
+      .get(`/games?name=${gameNames.join('&name=')}`)
+      .then((res) => res.data.data.map((game: Game) => game.id));
+  }
+
+  /**
+   * Retrieve an array of live streams for a given array of videogame IDs, optionally, only those with Twitch Drops enabled.
+   * @returns An array of videogame Twitch IDs.
+   * @param dropsEnabled Whether to only return streams with Twitch Drops enabled.
+   * @returns An array of live streams on Twitch.
+   */
+  async getLiveStreams(gameIds: string[], dropsEnabled = false): Promise<TwitchStream[]> {
+    const dropsEnabledTagId = 'c2542d6d-cd10-4532-919b-3d19f30a768b';
+
+    return this.client
+      .get(`/streams?game_id=${gameIds.join('&game_id=')}`)
+      .then((res) => res.data.data)
+      .then((streams) =>
+        dropsEnabled ? streams.filter((stream: TwitchStream) => stream.tag_ids.includes(dropsEnabledTagId)) : streams
+      );
+  }
+
+  /**
+   * Builds and returns the URL for a given stream.
+   * @param stream A Twitch stream from the Twitch API.
+   * @returns The URL to the livestream.
+   */
+  static buildTwitchURL(stream: TwitchStream): string {
+    return `https://www.twitch.tv/${stream.user_login}`;
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,31 @@
 import dotenv from 'dotenv';
-import axios, { AxiosInstance } from 'axios';
+import TwitchAPIClient from './TwitchAPIClient';
 
-// Import env variables
+// Import env variables from .env file
 dotenv.config();
-
-// Request and parse an access token
-async function getTwitchCredentials(clientId: string, clientSecret: string): Promise<any> {
-  return axios
-    .post(
-      'https://id.twitch.tv/oauth2/token',
-      `client_id=${clientId}&client_secret=${clientSecret}&grant_type=client_credentials`,
-      { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
-    )
-    .then((res) => res.data);
-}
-
-// Initialise a Twitch API client
-class TwitchAPIClient {
-  private client: AxiosInstance;
-
-  constructor(clientId: string, credentials: any) {
-    this.client = axios.create({
-      baseURL: 'https://api.twitch.tv/helix',
-      headers: {
-        Authorization: `Bearer ${credentials.access_token}`,
-        'Client-Id': clientId,
-      },
-    });
-  }
-
-  async getGameIds(gameNames: string[]): Promise<any[]> {
-    return this.client
-      .get(`/games?name=${gameNames.join('&name=')}`)
-      .then((res) => res.data.data.map((game: any) => game.id));
-  }
-
-  async getLiveStreams(gameIds: string[]): Promise<any[]> {
-    return this.client.get(`/streams?game_id=${gameIds.join('&game_id=')}`).then((res) => res.data);
-  }
-}
 
 async function main() {
   const clientId = process.env.TWITCH_CLIENT_ID!;
   const clientSecret = process.env.TWITCH_CLIENT_SECRET!;
   const gameNames = process.env.GAME_NAMES!.split('|');
 
-  const credentials = await getTwitchCredentials(clientId, clientSecret);
-  const client = new TwitchAPIClient(clientId, credentials);
+  const client = await TwitchAPIClient.create(clientId, clientSecret);
 
-  const liveStreams = await client.getLiveStreams(await client.getGameIds(gameNames));
-  console.log(liveStreams);
+  const liveStreams = await client.getGameIds(gameNames).then((gameIds) => client.getLiveStreams(gameIds, true));
+  if (liveStreams.length > 0) {
+    liveStreams.forEach((stream) => {
+      console.log(
+        `${stream.user_name} is playing ${stream.game_name} live at ${TwitchAPIClient.buildTwitchURL(stream)}!`
+      );
+    });
+  } else {
+    console.log(
+      [
+        `No one is live for the following games: ${gameNames}.`,
+        "Check the game names, disable the 'Drops Enabled' tag, or come back later.",
+      ].join('\n')
+    );
+  }
 }
 
 main();
