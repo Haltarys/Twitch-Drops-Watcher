fc4e3e58ab4c0b9e7565423c75c56a3a52112939
Haltarys <45515869+Haltarys@users.noreply.github.com>
2022-12-12 23:37:44 +0100

    feat(run.sh): improve script

--- run.sh
+++ run.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-# # Build if image does not exist
-if [[ "$(docker images -q twitch-drops-watcher 2> /dev/null)" == "" ]]; then
-  docker build -t twitch-drops-watcher .
+# Build if image does not exist
+if [[ "$(sudo docker images -q twitch-drops-watcher 2> /dev/null)" == "" ]]; then
+  sudo docker build -t twitch-drops-watcher .
 fi
 
-# # Run and log results
+# Run and log results
 logfile="$HOME/twitch-drops-watcher.log"
+gamenames=("Mortal Shell" "Mortal Shell: Enhanced Edition") # Or whatever games you prefer
 
 echo $(date) >> $logfile
-(docker run --rm twitch-drops-watcher "Mortal Shell" "Mortal Shell: Enhanced Edition" --drops 2>&1) >> $logfile
+(sudo docker run --rm twitch-drops-watcher $gamenames --drops 2>&1) >> $logfile
 echo >> $logfile
